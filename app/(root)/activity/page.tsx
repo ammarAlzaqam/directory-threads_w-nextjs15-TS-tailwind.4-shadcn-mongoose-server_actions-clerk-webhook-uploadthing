ed4c69dbc5411aff725ac3a3a70111b3deb2e45b
ammarAlzaqam <git@ammaralzaqam.com>
2025-09-17 12:54:30 +0300

    fix activity page to show msg if no activities

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,4 @@
-import {
-  fetchUser,
-  fetchUserPosts,
-  getActivities,
-} from "@/lib/actions/user.action";
+import { fetchUser, getActivities } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
@@ -18,33 +14,37 @@ export default async function ActivityPage() {
     <section>
       <h1 className="text-heading1-bold mb-14">Activity</h1>
 
-      <div className="flex flex-col gap-5">
-        {activities.map((activity) => (
-          <Link
-            href={`/thread/${activity.parentId}`}
-            key={activity._id}
-            className="px-4 xs:px-7 py-4 flex items-center gap-2 bg-dark-3"
-          >
-            {/*//! Author image */}
-            <div className="relative h-5 w-5">
-              <Image
-                src={activity.author.image}
-                alt="profile-img"
-                fill
-                className="rounded-full object-cover"
-              />
-            </div>
+      {activities.length > 0 ? (
+        <div className="flex flex-col gap-5">
+          {activities.map((activity) => (
+            <Link
+              href={`/thread/${activity.parentId}`}
+              key={activity._id}
+              className="px-4 xs:px-7 py-4 flex items-center gap-2 bg-dark-3"
+            >
+              {/*//! Author image */}
+              <div className="relative h-5 w-5">
+                <Image
+                  src={activity.author.image}
+                  alt="profile-img"
+                  fill
+                  className="rounded-full object-cover"
+                />
+              </div>
 
-            {/*//! Author name */}
-            <p className="text-small-medium text-primary-500">
-              {activity.author.name}
-            </p>
-            <p className="text-small-medium line-clamp-1">
-              replied to your thread
-            </p>
-          </Link>
-        ))}
-      </div>
+              {/*//! Author name */}
+              <p className="text-small-medium text-primary-500">
+                {activity.author.name}
+              </p>
+              <p className="text-small-medium line-clamp-1">
+                replied to your thread
+              </p>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p className="no-result">No activities</p>
+      )}
     </section>
   );
 }
